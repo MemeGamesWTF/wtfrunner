b62826e9b483ec5012b2d6a996d9412e0ce5ae81
Pasindu Promod <dev@ninehermits.com>
2024-12-17 12:25:57 +0530

    aaa

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1734418513,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/backgroundnew-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/backgroundnew2-sheet0.png",
+		"images/pickchris-sheet0.png",
+		"images/particles.png",
+		"images/trianglemorty-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
